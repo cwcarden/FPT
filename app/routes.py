6c45f7fd64f111e1758fb5a1602cc7551eca6db4
charles <charles@charlescarden.com>
2018-12-05 18:55:57 -0600

    antoher commit again. damn i'm on a roll

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,26 @@
 import os
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy 
-import sqlite3 
 
-app = Flask(__name__)
+app = Flask(__name__)   
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/prodplan'
 db = SQLAlchemy(app)
 
 class Fields(db.Model):
-    hybrid = db.Column(db.Text(80), primary_key=True, nullable=False)
-    grower = db.Column(db.Text(80), primary_key=True, nullable=False)
-    field_name = db.Column(db.Text(80), primary_key=True, nullable=False)
-    certified = db.Column(db.Text(80), primary_key=True, nullable=False)
+    hybrid = db.Column(db.String(80), primary_key=True)
+    grower = db.Column(db.String(80), primary_key=True)
+    field_name = db.Column(db.String(80), unique=True)
+    certified = db.Column(db.String(80), primary_key=True)
     field_number = db.Column(db.Integer, primary_key=True)
-    area = db.Column(db.Integer, primary_key=True, nullable=False)
-    cont_gross_acres = db.Column(db.Integer, primary_key=True, nullable=False)
-    percent_target = db.Column(db.Integer, primary_key=True, nullable=False)
-    female_plant_population = db.Column(db.Integer, primary_key=True, nullable=False)
-    hybrid_code = db.Column(db.Text, primary_key=True, nullable=False)
-    material_group = db.Column(db.Text, primary_key=True, nullable=False)
+    area = db.Column(db.Integer, primary_key=True)
+    cont_gross_acres = db.Column(db.Integer, primary_key=True)
+    percent_target = db.Column(db.Integer, primary_key=True)
+    female_plant_population = db.Column(db.Integer, primary_key=True)
+    hybrid_code = db.Column(db.String(80), primary_key=True)
+    material_group = db.Column(db.String(80), primary_key=True)
 
-    def __repr__(self, hybrid, grower, field_name, certified, field_number, area, cont_gross_acres, percent_target, female_plant_population, hybrid_code, material_group):
+    def __init__(self, hybrid, grower, field_name, certified, field_number, area, cont_gross_acres, percent_target, female_plant_population, hybrid_code, material_group):
         self.hybrid = hybrid
         self.grower = grower
         self.field_name = field_name 
@@ -31,6 +32,10 @@ class Fields(db.Model):
         self.female_plant_population = female_plant_population
         self.hybrid_code = hybrid_code
         self.material_group = material_group
+    
+    def __repr__(self):
+        return '<Fields %r>' % self.field_name
+
 
 @app.route('/')
 @app.route('/index')
@@ -58,17 +63,6 @@ def prod_budget():
 @app.route('/prod_reports')
 def prod_report():
     return render_template('prod_reports.html')
-
-@app.route('/plan')
-def plan():
-    c = sqlite3.connect('prodplan.db')
-    c.row_factory = sqlite3.Row  
-
-    c = c.cursor()
-    c.execute("SELECT * FROM fields")
-
-    rows = c.fetchone();
-    return render_template('plan.html', rows = rows)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import sqlite3
 
 conn = sqlite3.connect('prodplan.db')
-print("Opened database successfully")
+c = conn.cursor()
 
-conn.execute('CREATE TABLE fields (hybrid TEXT, grower TEXT, field_name TEXT, certified TEXT, field_number INTEGER, area INTEGER, cont_gross_acres INTEGER, percent_target INTEGER, female_plan_acres INTEGER, hybrid_code TEXT, material_group TEXT)')
+c.execute('CREATE TABLE fields (hybrid TEXT, grower TEXT, field_name TEXT, certified TEXT, field_number INTEGER, area INTEGER, cont_gross_acres INTEGER, percent_target INTEGER, female_plan_acres INTEGER, hybrid_code TEXT, material_group TEXT)')
 print("Table created successfully")
+
+conn.commit()
 conn.close()
