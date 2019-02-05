590f2e3fcd259487b7d1aa789b2bea62f8be4c65
equineranch <charles@charlescarden.com>
2019-02-05 15:05:33 -0600

    started report functions

---
+++ README.md
@@ -0,0 +1,4 @@
+Installation
+
+1. pipenv install (installs dependencies)
+2. navigate to app folder, python3.7, from app import db, db.create_all()
--- app/reports.py
+++ app/reports.py
@@ -1,62 +1,18 @@
 import sqlite3
-from fpdf import FPDF
+import csv
 
 #By Area 4 query
-def fp_area4_data():
+def seedfield():
     conn = sqlite3.connect('prodplan.db')
     c = conn.cursor()
-    c.execute('SELECT id, area, hybrid, grower, field_name, certified, field_number, cont_gross_acres, percent_target, female_plant_population, hybrid_code, material_group FROM Seedfield')
+    c.execute('SELECT * FROM Seedfield')
     data = c.fetchall()
-    for field in data:
-        if field[1] == 4:
-            print(field)
     conn.close()
 
-#By Area 5 query
-def fp_area5_data():
+def budget():
     conn = sqlite3.connect('prodplan.db')
     c = conn.cursor()
-    c.execute('SELECT id, area, hybrid, grower, field_name, certified, field_number, cont_gross_acres, percent_target, female_plant_population, hybrid_code, material_group FROM Seedfield')
-    data = c.fetchall()
-    for field in data:
-        if field[1] == 5:
-            print(field)
-    conn.close()
-
-#By Area 7 query
-def fp_area7_data():
-    conn = sqlite3.connect('prodplan.db')
-    c = conn.cursor()
-    c.execute('SELECT id, area, hybrid, grower, field_name, certified, field_number, cont_gross_acres, percent_target, female_plant_population, hybrid_code, material_group FROM Seedfield')
-    data = c.fetchall()
-    for field in data:
-        if field[1] == 7:
-            print(field)
-    conn.close()
-
-#By Area 8 query
-def fp_area8_data():
-    conn = sqlite3.connect('prodplan.db')
-    c = conn.cursor()
-    c.execute('SELECT id, area, hybrid, grower, field_name, certified, field_number, cont_gross_acres, percent_target, female_plant_population, hybrid_code, material_group FROM Seedfield')
-    data = c.fetchall()
-    for field in data:
-        if field[1] == 8:
-            print(field)
-    conn.close()
-
-#sort db data by grower
-def fp_grower_data():
-    conn = sqlite3.connect('prodplan.db')
-    c = conn.cursor()
-    c.execute('SELECT id, grower, hybrid, field_name, certified, field_number, area, cont_gross_acres, percent_target, female_plant_population, hybrid_code, material_group FROM Seedfield')
+    c.execute('SELECT * FROM Budget')
     data = c.fetchall()
     conn.close()
-    for fields in data:
-        lfields = list(fields)
-        print(lfields)
-        for index in lfields:
-            print(lfields[1])
-   
-    
 
