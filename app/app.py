c3a3f47034a4b4a3d5e5b11c3f6a0dcb9a0f0b6e
charles <charles@charlescarden.com>
2019-12-13 14:12:28 -0600

    fixed dev port

--- app/app.py
+++ app/app.py
@@ -129,4 +129,4 @@ def delete_budget_data():
 ###############################################################################
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=4000, debug=True)
